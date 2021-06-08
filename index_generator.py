f29fbc7b41534fa98f2b2d5f8328afffaa5b61d8
aesyklos07 <aesyklos07@gmail.com>
2021-06-08 23:58:27 +0530

    This Script generates index file

---
+++ index_generator.py
@@ -0,0 +1,12 @@
+import os
+
+files = ["<h2> Devnest Take-Home Assignments </h2>\n", "<h5> @Bit-Sahil04 </h5>\n"]
+
+for dir, _, _ in os.walk("."):
+	if 'git' not in dir and not dir == ".":
+		name = dir.split("\\")
+		html_link = f"<a href='{dir}'>{name[-1]}</a><br>\n"
+		files.append(html_link)
+	
+with open("index.html", 'w+') as f:
+	f.writelines(files)
