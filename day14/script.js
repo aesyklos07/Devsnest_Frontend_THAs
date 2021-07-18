c8c2085cd2fc764fc69678b708c1ddf4ba3607ec
Utkarsh Tiwari <64188176+aesyklos07@users.noreply.github.com>
2021-07-18 05:19:43 -0700

    Added day 14 THA

---
+++ day14/script.js
@@ -0,0 +1,17 @@
+let cli=document.querySelector('.cli');
+let hov=document.querySelector('.hov');
+// let cli=document.querySelector('.cli');
+// let cli=document.querySelector('.cli');
+
+cli.addEventListener('click',()=>{
+    cli.innerHTML="clicked";
+})
+hov.addEventListener('mouseover',()=>{
+    hov.innerHTML="maja aya ?";
+})
+hov.addEventListener('mouseout',()=>{
+    hov.innerHTML="Hover Me";
+})
+cli.addEventListener('click',()=>{
+    cli.innerHTML="clicked";
+})
